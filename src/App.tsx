369f8cd983c92b73dfbe5f30340677b31a9d56ee
unknown <ustchcl@gamil.com>
2022-04-07 10:55:01 +0800

    config react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import './App.css';
 import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" render={() => <Redirect to="/home" />}></Route>
+        {/**配置路由 */}
+        <Route path="/home" component={Home}></Route>
+      </div>
+    </Router>
   );
 }
 
