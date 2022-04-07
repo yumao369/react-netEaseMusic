24480a39e148d1af4b0d0a3c9e73d323548c6536
yumao369 <896115337@qq.com>
2022-04-07 20:18:36 +0800

    config ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
-import './App.css';
+import { Layout, Menu } from "antd"
 import Home from './pages/home';
+import styles from "./App.module.less"
+
+const { Header, Content, Footer } = Layout
+
+const menu = [{
+  label: '发现',
+  path: '/home'
+}, {
+  label: '歌单',
+  path: '/sheet'
+}]
 
 function App() {
+
+  const renderMenuItem = () => {
+    console.log(menu)
+    return <div>
+      {menu.map((item, index) => <Menu.Item>{item.label}</Menu.Item>)}
+    </div>
+  }
+
   return (
     <Router>
-      <div className="App">
+      <div className={styles.app}>
+        <Layout className={styles.layout}>
+          <Header className={styles.header}>
+            <div className={styles.wrap}>
+              <div className={styles.left}>
+                <h1 className={styles.logo}>Music</h1>
+                <Menu mode='horizontal' className={styles.mainMenu} theme='dark'>
+                  {renderMenuItem()}
+                </Menu>
+              </div>
+              <div className={styles.right}>
+                <div>search</div>
+                <div className={styles.member}>
+
+                </div>
+              </div>
+            </div>
+          </Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
         <Route exact path="/" render={() => <Redirect to="/home" />}></Route>
         {/**配置路由 */}
         <Route path="/home" component={Home}></Route>
