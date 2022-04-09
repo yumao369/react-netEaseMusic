14de57923cc205ca86e719928144b385d31b93b4
yumao369 <896115337@qq.com>
2022-04-09 18:32:24 +0800

    normal update

--- README.md
+++ README.md
@@ -44,3 +44,6 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+summary:
+1.Restart the project after adding .env file,or it may not work.
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { BASE_URL } from "../../utils/url";
+import { API } from "../../utils/api";
 import styles from "./index.module.less"
 
-export default function Home () {
+export default function Home() {
+
+  useEffect(() => {
+    getBanners();
+  })
+
+  const getBanners = async () => {
+    const res = await API.get('/banner');
+    const data = res.data;
+    console.log('bannerdata', data)
+  }
   return <div className={styles.home}>home</div>
 }
